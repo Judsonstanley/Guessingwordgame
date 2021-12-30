16571fac39de4d32a53b3a2a1dd0e803831c9cf5
Judsonstanley <96763636+Judsonstanley@users.noreply.github.com>
2021-12-30 10:42:23 +0530

    Add files via upload

--- Guessing word game.py
+++ Guessing word game.py
@@ -5,21 +5,17 @@ a=["apple","boy","bat","elephant","height","zoo","xylophone","no","open","jug","
 b = random.choice(a)
 print(b)
 # check if that alphabet is in the list
-
 for i in range(0,len(b)):
     c = input("Enter your guess")
     d = b[i][0]
     e = c[0]
-    print(e)
     print(d)
     if(d!=e):
         print("incorrect")
-    elif(d==e):
-        print("Next guess")
-        break;
-    else:
-        print("game over")
-        break;
+    elif d==e:
+        print("Correct")
+        continue
+print("gameover")
 
 
 
